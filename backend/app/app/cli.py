dcd4ad4326ca58ffe77d0ff68e3d64e971d61fc0
Alex Shamray <oleksiy.shamray@gmail.com>
2021-03-11 12:48:12 +0200

    Adding CLI application

---
+++ backend/app/app/cli.py
@@ -0,0 +1,15 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def changepassword(email: str = ""):
+    """
+    Change a user's password for app.models.user.User.
+    """
+    pass
+
+
+if __name__ == "__main__":
+    app()
