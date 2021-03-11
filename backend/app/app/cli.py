f46abecbd0cc87167a74e9544eb0bead8436bfe5
Alex Shamray <oleksiy.shamray@gmail.com>
2021-03-11 17:24:57 +0200

    Adding changepassword CLI command

--- backend/app/app/cli.py
+++ backend/app/app/cli.py
@@ -1,7 +1,9 @@
 import typer
+from pydantic import ValidationError
 
 from app import crud
 from app.db.session import SessionLocal
+from app.password_validation import validate_password
 
 app = typer.Typer(help="CLI user manager.")
 
@@ -10,12 +12,19 @@ app = typer.Typer(help="CLI user manager.")
 def createsuperuser(
     email: str = typer.Argument(
         ..., help="Specifies the login for the superuser."
-    ),
+    )
 ):
     """
     Used to create a superuser.
     """
     typer.echo(email)
+
+
+def _get_pass(prompt="Password"):
+    p = typer.prompt(prompt)
+    if not p:
+        raise typer.Abort()
+    return p
 
 
 @app.command()
@@ -29,7 +38,39 @@ def changepassword(
     """
     db = SessionLocal()
     user = crud.user.get_by_email(db, email=email)
-    typer.echo(user)
+    if not user:
+        typer.echo("user '%s' does not exist" % email)
+        raise typer.Exit(code=1)
+
+    typer.echo("Changing password for user '%s'" % user)
+
+    MAX_TRIES = 3
+    count = 0
+    p1, p2 = 1, 2  # To make them initially mismatch.
+    password_validated = False
+    while (p1 != p2 or not password_validated) and count < MAX_TRIES:
+        p1 = _get_pass()
+        p2 = _get_pass("Password (again)")
+        if p1 != p2:
+            typer.echo('Passwords do not match. Please try again.')
+            count += 1
+            # Don't validate passwords that don't match.
+            continue
+        try:
+            validate_password(p2, user)
+        except ValidationError as err:
+            typer.echo('\n'.join(err.messages), err=True)
+            count += 1
+        else:
+            password_validated = True
+
+    if count == MAX_TRIES:
+        typer.echo("Aborting password change for user '%s' after %s attempts" % (user, count), err=True)
+        raise typer.Exit(code=1)
+
+    crud.user.update(db, db_obj=user, obj_in={'password': p1})
+
+    typer.echo("Password changed successfully for user '%s'" % user)
 
 
 if __name__ == "__main__":
