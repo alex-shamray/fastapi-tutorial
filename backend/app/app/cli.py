8c401f5e44c92a7e55251b7df1e7c16ea29d4885
Alex Shamray <oleksiy.shamray@gmail.com>
2021-03-11 15:12:32 +0200

    Adding CLI Arguments with Help to createsuperuser and changepassword CLI commands

--- backend/app/app/cli.py
+++ backend/app/app/cli.py
@@ -1,22 +1,35 @@
 import typer
 
-app = typer.Typer()
+from app import crud
+from app.db.session import SessionLocal
+
+app = typer.Typer(help="CLI user manager.")
 
 
 @app.command()
-def createsuperuser():
+def createsuperuser(
+    email: str = typer.Argument(
+        ..., help="Specifies the login for the superuser."
+    ),
+):
     """
     Used to create a superuser.
     """
-    pass
+    typer.echo(email)
 
 
 @app.command()
-def changepassword(email: str = ""):
+def changepassword(
+    email: str = typer.Argument(
+        ..., help="Email to change password for."
+    )
+):
     """
     Change a user's password for app.models.user.User.
     """
-    pass
+    db = SessionLocal()
+    user = crud.user.get_by_email(db, email=email)
+    typer.echo(user)
 
 
 if __name__ == "__main__":
