86664c85352e3c5acc8264fce3054844e953f9c6
Alex Shamray <oleksiy.shamray@gmail.com>
2021-03-11 19:39:25 +0200

    Adding a password prompt in changepassword CLI command

---
+++ backend/app/app/password_validation.py
@@ -0,0 +1,8 @@
+def validate_password(password, user=None, password_validators=None):
+    """
+    Validate whether the password meets all validator requirements.
+
+    If the password is valid, return ``None``.
+    If the password is invalid, raise ValidationError with all error messages.
+    """
+    pass
