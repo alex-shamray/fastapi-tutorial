186254c7ca8485eafa06008d7bb683bf4c1947b9
Alex Shamray <oleksiy.shamray@gmail.com>
2021-03-12 09:59:10 +0200

    Adding password CLI Option Callback to validate password complexity

--- backend/app/app/cli.py
+++ backend/app/app/cli.py
@@ -3,7 +3,7 @@ from pydantic import ValidationError
 
 from app import crud
 from app.db.session import SessionLocal
-from app.password_validation import validate_password
+from app.schemas import UserUpdate
 
 app = typer.Typer(help="CLI user manager.")
 
@@ -20,13 +20,21 @@ def createsuperuser(
     typer.echo(email)
 
 
+def password_callback(ctx: typer.Context, value: str):
+    try:
+        UserUpdate(password=value)
+    except ValidationError as e:
+        raise typer.BadParameter('\n'.join([error['msg'] for error in e.errors()]))
+    return value
+
+
 @app.command()
 def changepassword(
     email: str = typer.Argument(
         ..., help="Email to change password for."
     ),
     password: str = typer.Option(
-        ..., prompt=True, confirmation_prompt=True, hide_input=True
+        ..., prompt=True, confirmation_prompt=True, hide_input=True, callback=password_callback
     ),
 ):
     """
@@ -39,12 +47,6 @@ def changepassword(
         raise typer.Exit(code=1)
 
     typer.echo("Changing password for user '%s'" % user)
-
-    try:
-        validate_password(password, user)
-    except ValidationError as err:
-        typer.secho('\n'.join(err.messages), fg=typer.colors.RED, err=True)
-        raise typer.Exit(code=1)
 
     crud.user.update(db, db_obj=user, obj_in={'password': password})
 
