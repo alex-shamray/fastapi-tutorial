d13d741619b262a6fecb96356df9e0176725b00b
Alex Shamray <oleksiy.shamray@gmail.com>
2021-03-12 11:07:04 +0200

    Adding password complexity validation to pydentic UserBase model

--- backend/app/app/schemas/user.py
+++ backend/app/app/schemas/user.py
@@ -1,6 +1,9 @@
+import gzip
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, validator
+
+from app.core.config import settings
 
 
 # Shared properties
@@ -9,6 +12,29 @@ class UserBase(BaseModel):
     is_active: Optional[bool] = True
     is_superuser: bool = False
     full_name: Optional[str] = None
+
+    @validator('password', check_fields=False)
+    def password_minimum_length(cls, v, values, **kwargs):
+        if len(v) < settings.PASSWORD_MIN_LENGTH:
+            raise ValueError(
+                f"This password is too short. It must contain at least {settings.PASSWORD_MIN_LENGTH} characters."
+            )
+        return v
+
+    @validator('password', check_fields=False)
+    def common_password(cls, v):
+        password_list_path = settings.DEFAULT_PASSWORD_LIST_PATH
+        try:
+            with gzip.open(password_list_path, 'rt', encoding='utf-8') as f:
+                passwords = {x.strip() for x in f}
+        except OSError:
+            with open(password_list_path) as f:
+                passwords = {x.strip() for x in f}
+        if v.lower().strip() in passwords:
+            raise ValueError(
+                "This password is too common.",
+            )
+        return v
 
 
 # Properties to receive via API on creation
