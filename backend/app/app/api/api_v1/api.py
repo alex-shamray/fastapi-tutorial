fa0901c1e62ba1c8e08dd4d617dec281371a5567
Alex Shamray <oleksiy.shamray@gmail.com>
2021-04-08 20:15:11 +0300

    Renaming endpoints to routers

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.routers import items, login, users, utils
+from .routers import items, login, users, utils
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
