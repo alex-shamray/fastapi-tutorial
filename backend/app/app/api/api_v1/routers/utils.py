c1c6f58190d9efbcba1a8c79cf0339839b423bc4
Alex Shamray <oleksiy.shamray@gmail.com>
2021-04-09 00:43:30 +0300

    Renaming deps module to dependencies

--- backend/app/app/api/api_v1/routers/utils.py
+++ backend/app/app/api/api_v1/routers/utils.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 from pydantic.networks import EmailStr
 
 from app import models, schemas
-from app.api import deps
+from app.api import dependencies as deps
 from app.core.celery_app import celery_app
 from app.utils import send_test_email
 
