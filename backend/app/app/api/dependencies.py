00cb86d8bbe40876ff06181ef3f24093e4f7f8de
Alex Shamray <oleksiy.shamray@gmail.com>
2021-05-16 12:29:11 +0300

    Adding additional header WWW-Authenticate with value Bearer to  HTTP (error) status code 401 "UNAUTHORIZED" to be compliant with the OAuth2 specifications.

--- backend/app/app/api/dependencies.py
+++ backend/app/app/api/dependencies.py
@@ -27,19 +27,21 @@ def get_db() -> Generator:
 def get_current_user(
     db: Session = Depends(get_db), token: str = Depends(reusable_oauth2)
 ) -> models.User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     try:
         payload = jwt.decode(
             token, settings.SECRET_KEY, algorithms=[security.ALGORITHM]
         )
         token_data = schemas.TokenPayload(**payload)
     except (jwt.JWTError, ValidationError):
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="Could not validate credentials",
-        )
+        raise credentials_exception
     user = crud.user.get(db, id=token_data.sub)
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise credentials_exception
     return user
 
 
