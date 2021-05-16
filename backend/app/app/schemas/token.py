c9e2eb0a0b7ab099e834894f88fc03b5eba4ede4
Alex Shamray <oleksiy.shamray@gmail.com>
2021-05-16 20:04:18 +0300

    Validating the JWT token payload with Pydantic to ensure that we have exactly a list of str with the scopes and an int with the user_id.

--- backend/app/app/schemas/token.py
+++ backend/app/app/schemas/token.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 
 from pydantic import BaseModel
 
@@ -9,4 +9,5 @@ class Token(BaseModel):
 
 
 class TokenPayload(BaseModel):
-    sub: Optional[int] = None
+    user_id: Optional[int] = None
+    scopes: List[str] = []
