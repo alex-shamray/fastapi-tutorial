f63284f8aae1bb8aa638d5f2f3cf0d385d40392a
Alex Shamray <oleksiy.shamray@gmail.com>
2022-06-06 10:05:29 +0300

    Extending health_check endpoint

--- backend/app/app/api/api_v1/routers/utils.py
+++ backend/app/app/api/api_v1/routers/utils.py
@@ -2,10 +2,12 @@ from typing import Any
 
 from fastapi import APIRouter, Depends
 from pydantic.networks import EmailStr
+from sqlalchemy.orm import Session
 
 from app import models, schemas
 from app import dependencies as deps
 from app.core.celery_app import celery_app
+from app.dependencies import get_db
 from app.utils import send_test_email
 
 router = APIRouter()
@@ -35,5 +37,10 @@ def test_email(
     return {"msg": "Test email sent"}
 
 
-def health_check():
-    return "OK"
+def health_check(db: Session = Depends(get_db)):
+    try:
+        db.execute('select 1')
+    except Exception:
+        return "Failing"
+    else:
+        return "OK"
